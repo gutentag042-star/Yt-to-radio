f9c17437b3d648d33750b91b1f2c0faa09c34d67
gutentag042-star <gutentag042@gmail.com>
2025-10-03 13:48:50 +0200

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,17 +1,15 @@
-```bash
 #!/bin/bash
 
-YouTube URL als Umgebungsvariable
-YOUTUBE_URL="YOUTUBE_URL"
+# === KONFIGURATION ===
+YOUTUBE_URL="https://m.youtube.com/watch?v=xf9Ejt4OmWQ&pp=ygUMU3Bpbm5pbiAyNCA3"
 
-Caster.fm Infos (aus deinem Bild)
-ICECAST_USER="source"
-ICECAST_PASS="VXpaCYCWQ"
-ICECAST_HOST="sapircast.caster.fm"
-ICECAST_PORT="18560"
-ICECAST_MOUNT="9OzKK"
+# Caster.fm Zugangsdaten
+CASTER_HOST="sapircast.caster.fm"
+CASTER_PORT="18560"
+CASTER_PASSWORD="VXpoCYCQWT"
+MOUNTPOINT="/9OzKK"  # Caster.fm nutzt meist "/stream" als Mountpoint
 
-Starte Stream
-yt-dlp -o - -f bestaudio "YOUTUBE_URL" | ffmpeg -i - -vn -c:a libmp3lame -b:a 96k -f mp3 \
-"icecast://ICECAST_USER:ICECAST_PASS@ICECAST_HOST:ICECAST_PORT/$ICECAST_MOUNT"
-```
+# === STREAM STARTEN ===
+yt-dlp -f bestaudio -o - "$YOUTUBE_URL" | ffmpeg -re -i - \
+  -vn -acodec libmp3lame -b:a 128k -content_type audio/mpeg \
+  -f mp3 "icecast://source:$CASTER_PASSWORD@$CASTER_HOST:$CASTER_PORT/$MOUNTPOINT"
